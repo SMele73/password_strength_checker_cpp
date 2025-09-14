d83e29e70a55c5b6362dcf98a68b1ce1e887364a
Shawn <cxd17@txstate.edu>
2025-09-14 16:00:04 -0500

    Added user feedback on passwords and grading system

--- main.cpp
+++ main.cpp
@@ -20,12 +20,11 @@ int main(){
 	std::cout << "Enter a password: \n";
 	std::cin >> password;
 
-	int score = scorePassword(password);
-	std::string category = passwordCategory(score);
-
 	std::cout << "Passwords are categorized based on their value.\n" 
 		<< "Weak < 40, Moderate 40-70, Strong above 70.\n"
-		<< "How did your password fare ? \n";
+		<< "How did your password rank? \n\n";
+	int score = scorePassword(password);
+	std::string category = passwordCategory(score);
 	std::cout << "Score: " << score << std::endl;
 	std::cout << "Category: " << category << std::endl;
 
@@ -65,16 +64,44 @@ int scorePassword(const std::string &password){
 	int score = 0;
 
 	//length points
-	if (password.length() >= 12)	 score += 40;
-	else if (password.length() >= 8) score += 20;
-	else score += 0;
-	
+	if (password.length() >= 15) {
+		score += 40;
+		std::cout << "+40: Excellent length (15+ characters).\n";
+	}
+	else if (password.length() >= 12) {
+		score += 20;
+		std::cout << "+20: Good length (12+ characters).\n";
+	}
+	else if (password.length() >= 8) {
+		score += 10;
+		std::cout << "+10: Okay length (8-11 characters).\n";
+	}
+	else if (password.length() >= 5) {
+		score += 0;
+		std::cout << "+0: Poor length (5-7 characters).\n";
+	}
+	else {
+		std::cout << "Automatic fail. Password is too short.\n";
+		return 0;
+	}
 
 	//character variety
-	if (hasLowercase(password)) score +=15;
-	if (hasUppercase(password)) score +=15;
-	if (hasDigit(password))		score +=15;
-	if (hasSymbol(password))	score +=15;
+	if (hasLowercase(password)) {
+		score +=15;
+		std::cout << "+15: Contains lowercase.\n";
+		}
+	if (hasUppercase(password)) {
+		score += 15;
+		std::cout << "+15: Contains uppercase.\n";
+	}
+	if (hasDigit(password)) {
+		score += 15;
+		std::cout << "+15: Contains a number.\n";
+	}
+	if (hasSymbol(password)) {
+		score += 15;
+		std::cout << "+15: Contains a symbol.\n";
+	}
 
 	return score;
 }
@@ -82,7 +109,7 @@ int scorePassword(const std::string &password){
 std::string passwordCategory(int score){
 	std::string category;
 	if (score < 40) category = "Weak";
-	else if (score < 70) category = "Moderate";
+	else if (score <= 70) category = "Moderate";
 	else category = "Strong";
 
 	return category;
